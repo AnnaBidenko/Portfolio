363d0132a1eb5db42b235fe572b9afd8c0a4e130
AnnaBidenko <anna.bidenko16@gmail.com>
2022-11-01 16:44:08 +0200

    bootstrap 4.6

---
+++ page/Arni Education/arni education/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+    $('.opinion__slider').bxSlider({
+        auto: true,
+        controls: true,
+        responsive: true,
+        speed: 1000,
+        pager: false,
+        mimSlides: 1,
+        maxSlides: 5,
+        moveSlides: 1,
+        slideWidth: 365
+    });
+});
