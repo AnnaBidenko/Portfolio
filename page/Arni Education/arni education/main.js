a0c494dff31ec07e4c54b2aa599cdea9c4092c88
AnnaBidenko <anna.bidenko16@gmail.com>
2022-11-14 10:50:05 +0200

    slider

--- page/Arni Education/arni education/main.js
+++ page/Arni Education/arni education/main.js
@@ -11,4 +11,11 @@ $(document).ready(function() {
         moveSlides: 1,
         slideWidth: 365
     });
+
+    $('.teacher_slider').bxSlider({
+        auto: true,
+        controls: false,
+        responsive: true,
+        speed: 1000
+    });
 });
